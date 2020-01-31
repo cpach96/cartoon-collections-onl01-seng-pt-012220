8d8f7611bf7d0c786f00d6a42097775ba3692b55
Curtis pachtinger <cpach96@gmail.com>
2020-01-31 16:34:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer)
     planeteer.map! {|c| c.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
 end
 
 def find_the_cheese# code an argument here
