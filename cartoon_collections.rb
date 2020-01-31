6c1fb8338cc0360b68e888f9fe8d1b47116ca15e
Curtis pachtinger <cpach96@gmail.com>
2020-01-31 16:42:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,9 @@ def summon_captain_planet(planeteer)
 end
 
 def long_planeteer_calls(calls_short)
-  if calls.any? {|i| i > 4}
+  if calls_short.any? {|i| i > 4}
     
   else
-    false
     
   end
 end
